5d1deca9524e6df932c98e7a7d90b3749f9c8ed6
Sidd <sid.dhakal@gmail.com>
2016-05-16 21:23:34 -0500

    list prints with event type

--- lib/udacilist.rb
+++ lib/udacilist.rb
@@ -42,7 +42,7 @@ class UdaciList
     puts @title
     puts "-" * @title.length
     @items.each_with_index do |item, position|
-      puts "#{position + 1}) #{item.details}"
+      puts "#{position + 1}) (#{item.item_type[0]}) #{item.details}"
     end
   end
 
